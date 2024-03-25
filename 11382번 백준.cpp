27cf47b0b8a2e84640211ac6297c0c9bd86be69e
shinjeongdong <qpal1829@naver.com>
2024-03-25 12:56:18 +0900

    1주차 과제

---
+++ 11382번 백준.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#pragma warning(disable:4996)
+
+int main() {
+
+    long long a, b, c;
+
+    scanf("%lld %lld %lld", &a, &b, &c);
+
+    printf("%lld", a + b + c);
+
+    return 0;
+}
